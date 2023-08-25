9f7baad78395e6f034d9f8450d0fb6c771b4e667
esraa mosaad <esraa.m.mosaad@gmail.com>
2023-08-25 04:36:35 +0300

    create custom notes item

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'custom_appbar.dart';
+import 'note_item.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.only(top: 32.0,left: 16,right: 16),
+      padding: EdgeInsets.only(top: 32.0, left: 8, right: 8),
       child: Column(
-
-
         children: [
           CustomAppBar(),
-
-
-
+          SizedBox(
+            height: 15,
+          ),
+          NotesItem(),
         ],
       ),
     );
   }
 }
-
-
-
-
-

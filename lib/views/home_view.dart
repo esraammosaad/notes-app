99be2ab29f6b24d5cec35ef005279992c8b109ae
esraa mosaad <esraa.m.mosaad@gmail.com>
2023-08-25 03:22:00 +0300

    making home page

---
+++ lib/views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/home_view_body.dart';
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: HomeViewBody(),
+    );
+  }
+}
